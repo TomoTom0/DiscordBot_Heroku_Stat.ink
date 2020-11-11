8492333571370a1b9a054be5b070037535aa44c1
Tomotom0 <abc.pcc.q@gmail.com>
2020-11-11 21:53:52 +0900

    auto commit

--- src/splat.py
+++ src/splat.py
@@ -55,8 +55,8 @@ class Splat(commands.Cog):
     async def showIksmAcc(self, ctx: commands.Context):
         """登録されているnintendoアカウント一覧を表示します。"""
         before_config_tmp=json.loads(os.getenv("iksm_configs", "{}"))
-        print(before_config_tmp, type(before_config_tmp), type(eval(before_config_tmp)) ,type(json.loads(os.getenv("iksm_configs", "{}"))))
-        before_config_jsons=eval(json.loads(os.getenv("iksm_configs", "{}"))) if type(before_config_tmp)=="str" else before_config_tmp
+        print(before_config_tmp, type(before_config_tmp), type(eval(before_config_tmp)) ,type(eval(before_config_tmp) if type(before_config_tmp)=="str" else before_config_tmp))
+        before_config_jsons=eval(before_config_tmp) if type(before_config_tmp)=="str" else before_config_tmp
         acc_names = [k for k, v in before_config_jsons.items() ]
         await ctx.channel.send(f"There is {len(acc_names)} accounts.\n----\n"+"\n".join(acc_names))
 
